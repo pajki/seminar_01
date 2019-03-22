542f469837290d350da8a6b280d4cc3a2a89cdf3
iztok <ir8617@student.uni-lj.si>
2019-03-22 12:54:36 +0100

    add_url modifications

--- sites/helpers/Frontier/Frontier.py
+++ sites/helpers/Frontier/Frontier.py
@@ -1,10 +1,9 @@
 from queue import LifoQueue
 from urllib.parse import urldefrag, urlparse
-from django.db import IntegrityError
 
 from sites.helpers.Downloader.HttpDownloader import HttpDownloader
 
-from sites.models import Page, Link, Site
+from sites.models import Page, Link, Site, PageType
 
 
 class Frontier:
@@ -20,29 +19,41 @@ class Frontier:
         :param from_page: id of page which url was found on
         :return: returns boolean value that indicates weather url was added to frontier
         """
+
+        # Remove fragment and add http:// if no scheme in url
         new_url = urldefrag(new_url).url
-        if Page.objects.exists(url=new_url):
+        if not urlparse(new_url).scheme:
+            new_url = "http://" + new_url
+
+        if Page.objects.filter(url=new_url).exists():
             return False
+
+        # TODO .gov.si check
+        # TODO canonical check
+        # TODO duplicate invhash check
 
         netloc = urlparse(new_url).netloc
 
         # Create new site if it doesn't exist yet
-        if not Site.objects.exists(domain=netloc):
-            site = Site(domain=netloc, sitemap_content=self.http_downloader.get_sitemap_for_url(netloc, True),
-                        robots_content=self.http_downloader.get_robots_file(netloc, True))
-            site.save()
-        else:
+        try:
             site = Site.objects.get(domain=netloc)
+        except Site.DoesNotExist:
+            site = Site(domain=netloc,
+                        sitemap_content=self.http_downloader.get_sitemap_for_url("http://" + netloc, True),
+                        robots_content=self.http_downloader.get_robots_file("http://" + netloc, True))
+            site.save()
+
+        page_type = PageType.objects.get(code="FRONTIER")
+        page = Page(url=new_url, site=site, page_type_code=page_type)
+        page.save()
 
         try:
-            page = Page.objects.create(url=new_url, site_id=site.id)
-            link = Link.objects.create(from_page=from_page, to_page=page.id)
+            link = Link(from_page=from_page, to_page=page)
+            link.save()
+        except ValueError:
+            print("ERR: from_page must be valid Page instance")
 
-            self.queue.put((page.url, page.id))
-
-        except IntegrityError as e:
-            print(e)
-            return False
+        self.queue.put(page)
 
         return True
 
